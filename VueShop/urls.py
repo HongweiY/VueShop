ea992e28f5d5ff4fd238cab96ee1ee5d82075c95
ymfsder <ymfsder@163.com>
2017-11-22 14:11:37 +0800

    添加支付宝支付接口

--- VueShop/urls.py
+++ VueShop/urls.py
@@ -28,6 +28,7 @@ from users.views import SmsCodeViewSet, UserViewSet
 from user_operation.views import UserFavViewSet, LeavingMessageViewSet, UserAddressViewSet
 from trade.views import ShoppingCartViewSet, OrderInfoViewSet, AliPayView
 from django.views.generic import TemplateView
+
 router = DefaultRouter()
 
 # 配置商品的URL
@@ -56,7 +57,7 @@ urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
 
     url(r'^', include(router.urls)),
-    url(r'^index/', TemplateView.as_view(template_name='index.html')),
+    url(r'^index/', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
 
--- apps/trade/views.py
+++ apps/trade/views.py
@@ -4,6 +4,7 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework import mixins
 from rest_framework.response import Response
+from django.shortcuts import redirect
 
 from utils.permissions import IsOwnerOrReadOnly
 from .serializers import ShoppingCartSerializer, ShoppingCartDetailSerializer, OrderSerializer, OrderDetailSerializer
@@ -11,6 +12,7 @@ from .models import ShoppingCart, OrderInfo, OrderGoods
 from rest_framework.views import APIView
 from utils.alipay import AliPay
 from VueShop.settings import private_key_path, alipay_key_path
+import datetime
 
 
 # Create your views here.
@@ -98,7 +100,12 @@ class AliPayView(APIView):
                 existed_order.pay_time = datetime.now()
                 existed_order.save()
 
-            return Response("success")
+            response = redirect('index')
+            response.set_cookie('nextPath', 'pay', max_age=2)
+            return response
+        else:
+            response = redirect('index')
+            return response
 
     def post(self, request):
         """
